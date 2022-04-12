3ba3ef4a3e8c7c5ae2b7af15e644fe02c12a76b2
Emanuel Silva-Lopez <emanuelsl@Emanuels-MacBook-Pro.local>
2022-04-12 10:58:21 -0400

    create connection to mysql and fixed package json

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const inquirer = require('inquirer');
-const db = require('./db/connection');
-const cTable = require('console.table');
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+const db = require(".");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "process.env.DB_PW",
+  database: "company"
+});
+
