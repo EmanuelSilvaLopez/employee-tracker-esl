3f20cfe1acb1a3907f339404bd5fc8c8d577cca0
EmanSL <emanuelsilvalopez@gmail.com>
2022-04-11 20:42:34 -0400

    fixed errors in syntax

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,32 +1,34 @@
-INSERT INTO departments
-  (department)
+INSERT INTO departments (name)
 VALUES
-('Directors'),
-('Management'),
-('Team')
+  ('Director'),
+  ('Management'),
+  ('Team');
 
-INSERT INTO roles
-  (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-('Store Director', 100000, 1)
-('Department Lead',50000, 1)
+    
+  ('Store Director', 30000, 1),
 
-('Softlines Manager', 30000, 2)
-('Hardlines Manager', 30000, 2)
-('Grocery Manager', 30000, 2)
+  ('Softlines', 25000, 2),
+  ('Hardlines', 35000, 2),
+  ('Grocery', 27500, 2),
 
-('Cart Attendant', 20000, 3)
-('Guest Advocate', 20000, 3)
-('DU Captain', 20000, 3)
+  ('Cart Attendant', 27500, 3),
+  ('Stocker', 25000, 3),
+  ('Folder', 50000, 3);
 
-INSERT INTO employees
-  (first_name, last_name, roles_id, manager_id)
-VALUES
-('Sarah', 'Grabbe', 1, NULL),
-('Glenn', 'Mccoy', 2, 1),
-('Surrey', 'Valdez', 2, 1),
-('Zack', 'Bell', 2, 1),
-('Manny', 'Lopez', 3, 2),
-('Morgan', 'Bro', 3, 2),
-('Jaer', 'Medina', 3, 4),
-('Joe', 'Hughes', 3, 3)
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+VALUES 
+  ('Francis', 'Smith', 1, NULL),
+
+  ('Jasmine', 'Rodriguez', 2, 1),
+
+  ('Lucille', 'Brown', 3, 1),
+
+  ('Khadeeja', 'Simmons', 4, 1),
+
+  ('Evelyn', 'Sanchez', 5, 2),
+
+  ('Buba', 'Smith', 6, 3),
+
+  ('Chad', 'Gilbert', 7, 4);
