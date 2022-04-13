971ec9fbd01c3d3e0bb69ef6ae5b1550265b2cd4
EmanSL <emanuelsilvalopez@gmail.com>
2022-04-13 19:37:14 -0400

    setup all functions to be used to satisfy challenge requirements

--- app.js
+++ app.js
@@ -17,3 +17,58 @@ const connection = mysql.createConnection({
   database: "company"
 });
 
+
+function prompt() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        promptMessages.viewAllEmployees,
+        promptMessages.viewAllDepartments,
+        promptMessages.viewAllRoles,
+        promptMessages.addEmployee,
+        promptMessages.updateEmployeeRole,
+        promptMessages.addDepartment,
+        promptMessages.addRole,
+        promptMessages.exit
+      ]
+    })
+    .then(answer => {
+      console.log('answer', answer);
+      switch (answer.action) {
+        case promptMessages.viewAllEmployees:
+          viewAllEmployees();
+          break;
+
+        case promptMessages.viewAllDepartments:
+          viewAllDepartments();
+          break;
+
+        case promptMessages.viewAllRoles:
+          viewAllRoles();
+          break;
+
+        case promptMessages.addEmployee:
+          addEmployee();
+          break;
+
+        case promptMessages.updateEmployeeRole:
+          updateEmployeeRole();
+          break;
+
+        case promptMessages.addRole:
+          addRole();
+          break;
+
+        case promptMessages.addDepartment:
+          addDepartment();
+          break;
+
+        case promptMessages.exit:
+          connection.end();
+          break;
+      }
+    });
+}
